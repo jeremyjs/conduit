5eb10c9350247b975013e03753389e6fc94d4baa
Jeremy Meyer <jmeyer1@enova.com>
2014-06-26 16:13:10 -0500

    fixed panel styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
   def dashboard
-    @widgets = [{id: 1, name: "Name", data: ""}, {id: 2, name: "Name", data: ""}, {id: 3, name: "Name", data: ""}]
+    @widgets = [
+      {id: 1, name: "Name", col: 1, row: 1, x: 6, y: 4, data: ""},
+      {id: 2, name: "Name", col: 1, row: 1, x: 4, y: 4, data: ""},
+      {id: 3, name: "Name", col: 1, row: 1, x: 4, y: 3, data: ""},
+      {id: 4, name: "Name", col: 1, row: 1, x: 4, y: 2, data: ""},
+      {id: 5, name: "Name", col: 1, row: 1, x: 3, y: 3, data: ""},
+      {id: 6, name: "Name", col: 1, row: 1, x: 3, y: 2, data: ""},
+      {id: 7, name: "Name", col: 1, row: 1, x: 2, y: 2, data: ""},
+      {id: 8, name: "Name", col: 1, row: 1, x: 2, y: 1, data: ""},
+      {id: 9, name: "Name", col: 1, row: 1, x: 1, y: 1, data: ""}
+    ]
   end
 end
 
