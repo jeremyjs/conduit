fbf5067a7b022c51a204a667cbe5d4f0ca6dfc8e
Namrata Prabhu <nprabhu@enova.com>
2014-07-02 17:12:46 -0500

    modified query.rb

--- app/models/query.rb
+++ app/models/query.rb
@@ -5,21 +5,25 @@ class Query < ActiveRecord::Base
   has_many :query_tables
 
   def execute
+    queries = []
     conn = PG.connect(host: 'qassdb-27-nut.cashnetusa.com', port: 5432, dbname: 'cnuapp_prod_uk', user: 'cnuapp', password: 'cnuappukqa')
     conn.exec(self.command) do |result|
       result.each do |row|
-        puts row
+        queries << row
       #  puts "%s\t%s" % row.values_at('id', 'email')
       end
     end
+    queries
   end
 
   def self.execute(command)
+    queries = []
     conn = PG.connect(host: 'qassdb-27-nut.cashnetusa.com', port: 5432, dbname: 'cnuapp_prod_uk', user: 'cnuapp', password: 'cnuappukqa')
     conn.exec(command) do |result|
       result.each do |row|
-        puts row
+        queries << row
       end
     end
+    queries
   end
 end
