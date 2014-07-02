070d9e8ef2da00191faa8b77ac0cb88882db9c41
Namrata Prabhu <nprabhu@enova.com>
2014-07-02 11:01:19 -0500

    add email field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
 
 
   validates_presence_of   :password, :on=>:create
-   validates_confirmation_of   :password, :on=>:create
-    validates_length_of :password, :within => Devise.password_length
+  validates_confirmation_of   :password, :on=>:create
+  validates_length_of :password, :within => Devise.password_length
+  validates_presence_of :email, :on => :create
+  validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => true, :if => :email_changed?
+  validates_format_of :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
 end
