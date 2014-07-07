cfde298e9b525f523216865b5bfe47851a690ce0
Jeremy Meyer <jmeyer1@enova.com>
2014-07-07 17:34:23 -0500

    changed grid granularity

--- db/seeds.rb
+++ db/seeds.rb
@@ -977,22 +977,22 @@ puts q.errors.full_messages
 q = Query.find_or_create_by(command: pitch_main_query_backup_0211)
 puts q.errors.full_messages
 
-g = Graph.find_or_create_by(name: "Test Graph", height: 2, width: 3)
+g = Graph.find_or_create_by(name: "Test Graph", height: 4, width: 6)
 g.page = 1
 g.save
 puts g.errors.full_messages
 
-g = Graph.find_or_create_by(name: "Test Graph 2", height: 4, width: 4)
+g = Graph.find_or_create_by(name: "Test Graph 2", height: 8, width: 8)
 g.page = 3
 g.save
 puts g.errors.full_messages
 
-g = Graph.find_or_create_by(name: "Test Graph 3", height: 2, width: 3)
+g = Graph.find_or_create_by(name: "Test Graph 3", height: 4, width: 6)
 g.page = 3
 g.save
 puts g.errors.full_messages
 
-qt = QueryTable.find_or_create_by(name: "Test Query Table", height: 4, width: 4)
+qt = QueryTable.find_or_create_by(name: "Test Query Table", height: 8, width: 8)
 qt.query = Query.first
 qt.page = 1
 qt.save
