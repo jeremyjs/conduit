adf7bbc4abb6904e8adb958cc03919aa46c217c8
Namrata Prabhu <nprabhu@enova.com>
2014-07-15 17:41:03 -0500

    changes to query and widget

--- app/models/query.rb
+++ app/models/query.rb
@@ -3,21 +3,16 @@ class Query < ActiveRecord::Base
 
   has_many :widgets
 
-  def initialize(attributes = {})
-    super
-    #initialize_variables_hash
-  end
 
  # def has_changed
   #  if self.command_changed? || self.variables_changed?
    #   self.execute
    # end
+  #
  # end
   def has_changed
     #rerun all widgtes when the command of a query changes
   end
-
-
 
   def self.execute(command)
     conn = PG.connect(host: 'slavedb2.quickquid.co.uk', port: 5432, dbname: 'cnuapp_prod_uk', user: 'conduit', password: 'cro0sSb@r')
@@ -28,13 +23,4 @@ class Query < ActiveRecord::Base
     self.command.gsub(/^$\n/, '').gsub(/^\s*--\s*/, '').lines.first.chomp
   end
 
-  private
- # def extract_variable_names
-   # self.command.nil? ?  [] : self.command.scan(/\%{(.*?)}/).flatten
- # end
- # def initialize_variables_hash
-   # extract_variable_names.each do |variable|
-     # self.variables[variable] = nil
-   # end
- # end
 end
