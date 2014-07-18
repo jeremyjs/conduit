b749d578c3f532504c94d352ec4d7a5fa0584650
Nathan Handler <nhandler@enova.com>
2014-07-18 09:52:34 -0500

    Prevent cron from sending mail due to STDOUT

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :output, "log/cron_log.log"
 set :environment, 'development'
 
 job_type :rbenv_rake, %Q{eval "$(rbenv init -)"; \
-                         cd :path && bundle install &&  bundle exec rake :task :output }
+                         cd :path && bundle install --quiet &&  bundle exec rake :task :output }
 
 every 15.minutes do
   rbenv_rake "conduit:execute_queries"
