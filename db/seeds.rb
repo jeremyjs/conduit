757ec0efb083b08467be05bbd5bf2e9de870c3e1
Namrata Prabhu <nprabhu@enova.com>
2014-07-28 16:10:56 -0500

    all graphs and tables have names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1009,16 +1009,16 @@ g.user = u
 g.save
 puts g.errors.full_messages
 
-g = Graph.find_or_create_by(name: "Test Graph 3", height: 5, width: 7)
+g = Graph.find_or_create_by(name: "Comparing t3uk and eloansuk on total sent volume", height: 5, width: 7)
 g.page = 3
 g.query_id = 4
 g.variables = {brand_id: "2", start_time: "2013-05-26 00:00:00", end_time: "2013-06-02 23:59:59", providers: "'eloansuk', 't3uk'"}
-g.display_variables = {kpis: ["total_sent", "total_imported"]}
+g.display_variables = {kpis: ["total_sent"]}
 g.user = u
 g.save
 puts g.errors.full_messages
 
-t = Table.find_or_create_by(name: "Query 4 Table", height: 4, width: 7)
+t = Table.find_or_create_by(name: "Pitch Main Performance for t3uk", height: 4, width: 7)
 t.query_id = 4
 t.page = 1
 t.variables = {brand_id: "2", start_time: "2013-05-28 00:00:00", end_time: "2013-05-30 23:59:59", providers: "'t3uk'"}
