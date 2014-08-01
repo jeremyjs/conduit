8176d75b132d711c901c7857762fde7342b9230e
Kevin Shu <kshu@enova.com>
2014-08-01 11:25:37 -0500

    added user creation ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
     @non_enova_users = User.all - User.all.enova_users
     @roles = Role.all
   end
+
+  def add_user
+    @user = User.new({email: params[:email], login: params[:login], password: params[:password], password_confirmation: [:password_confirmation]})
+    respond_to do |format|
+      if @user.save(validate: false)
+        format.json { render json: @user.errors}
+        format.js { render json: @user.errors}
+      else
+        format.json { render json: @user.errors}
+        format.js { render json: @user.errors}
+      end
+    end
+  end
 end
