6cd6fb9c1e1a6fb743881a7ad60b66f4fd59312b
Namrata Prabhu <nprabhu@enova.com>
2014-08-01 13:10:56 -0500

    refactored tests and seeds file

--- spec/models/complete_query_spec.rb
+++ spec/models/complete_query_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 RSpec.describe CompleteQuery, :type => :model do
 
+    let(:first_query) {FactoryGirl.create(:query, command: "SELECT id FROM customers LIMIT 5")}
+    let(:second_query) {FactoryGirl.create(:query, command: "SELECT id FROM customers LIMIT 10")}
+    let(:first_widget) {FactoryGirl.create(:widget , query_id: first_query.id)}
+    let(:complete_query_first) {CompleteQuery.find_by(query_id: first_widget.query_id)}
+
     before :each do
-      @first_query = FactoryGirl.create(:query, command: "SELECT id FROM customers LIMIT 5")
-      @second_query = FactoryGirl.create(:query, command: "SELECT id FROM customers LIMIT 10")
-      @first_widget = FactoryGirl.create(:widget , query_id: @first_query.id)
-      @complete_query_first = CompleteQuery.find_by(query_id: @first_query.id)
+      complete_query_first.query_id = first_query.id
     end
 
-    it "should be created once a query which is being executedfor the first time has finished" do
-      expect(@complete_query_first.variables).to eq(@first_widget.variables)
-      expect(@complete_query_first.query_result).to eq(@first_widget.query_result)
-      expect(@complete_query_first.last_executed).to eq(@first_widget.last_executed)
+    it "should be created once a query which is being executed for the first time has finished" do
+      expect(complete_query_first.variables).to eq(first_widget.variables)
+      expect(complete_query_first.query_result).to eq(first_widget.query_result)
+      expect(complete_query_first.last_executed).to eq(first_widget.last_executed)
     end
 
     it "belongs to the query whose query id it has" do
-      expect(@first_query.complete_queries.first).to eq(@complete_query_first)
+      expect(first_query.complete_queries.first).to eq(complete_query_first)
     end
 
     it "should be the only completed query in response to a single query" do
-      completed_queries = CompleteQuery.where(query_id: @first_query.id)
+      completed_queries = CompleteQuery.where(query_id: first_query.id)
       expect(completed_queries.count).to eq(1)
     end
-
 
 end
