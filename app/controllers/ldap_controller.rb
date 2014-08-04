7c610aa76e1e73dc551bc7ba71ba11953cbd5e5d
Kevin Shu <kshu@enova.com>
2014-08-04 12:13:53 -0500

    added ability to delete users, update user role mapping, update ldap mappings, delete ldap mappings

--- app/controllers/ldap_controller.rb
+++ app/controllers/ldap_controller.rb
@@ -5,4 +5,30 @@ class LdapController < ApplicationController
       format.json { render json: @groups.to_json }
     end
   end
+
+  def create
+    @ldap_role_mapping = LdapRoleMapping.new(ldap_group: params['ldap_group'], role: params['role'])
+    respond_to do |format|
+      if @ldap_role_mapping.save
+        format.json { render json: @ldap_role_mapping.errors}
+        format.js { render json: @ldap_role_mapping.errors}
+      else
+        format.json { render json: @ldap_role_mapping.errors}
+        format.js { render json: @ldap_role_mapping.errors}
+      end
+    end
+  end
+
+  def destroy
+    @ldap_role_mapping = LdapRoleMapping.find(params['mapping_id'])
+    respond_to do |format|
+      if @ldap_role_mapping.destroy!
+        format.json { render json: @ldap_role_mapping.errors}
+        format.js { render json: @ldap_role_mapping.errors}
+      else
+        format.json { render json: @ldap_role_mapping.errors}
+        format.js { render json: @ldap_role_mapping.errors}
+      end
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,44 @@ class UsersController < ApplicationController
   def index
     @non_enova_users = User.all - User.all.enova_users
     @roles = Role.all
+    @ldap_groups = User.get_all_ldap_groups
+    @ldap_role_mappings = LdapRoleMapping.all
   end
 
   def add_user
     @user = User.new({email: params[:email], login: params[:login], password: params[:password], password_confirmation: [:password_confirmation]})
     respond_to do |format|
       if @user.save(validate: false)
+        format.json { render json: @user.errors}
+        format.js { render json: @user.errors}
+      else
+        format.json { render json: @user.errors}
+        format.js { render json: @user.errors}
+      end
+    end
+  end
+
+  def update_mappings
+    params['mappings'].each do |email, roles|
+      puts "adding to #{email}"
+      @user = User.find_by(email: email)
+      @user.roles = []
+      roles.each do |role_name|
+        unless role_name == 'nil'
+          @user.add_role role_name.to_sym
+        end
+      end
+    end
+    respond_to do |format|
+      format.json { render json: @user.errors}
+      format.js { render json: @user.errors}
+    end
+  end
+
+  def destroy
+    @user = User.find_by(email: params['email'])
+    respond_to do |format|
+      if @user.destroy!
         format.json { render json: @user.errors}
         format.js { render json: @user.errors}
       else
